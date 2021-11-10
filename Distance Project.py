b26039ddea2c65ce0329ad9eedb5ebabefcc3a22
Deepika C <93979075+DeepikaC01@users.noreply.github.com>
2021-11-10 12:03:40 +0530

    Add files via upload

---
+++ Distance Project.py
@@ -0,0 +1,15 @@
+# Distance = int(input("Input the distance travelled: "))
+# Unit = input("Input Type i.e K for KMS or M for Miles: ")
+# if Unit.upper() == "K":
+#     Converted = Distance/1.609
+#     print(f"You have covered {Converted} Miles")
+# elif Unit.upper() == "M":
+#     Converted = Distance*1.609
+#     print(f"You have covered {Converted} KMS")
+# else:
+#     print("Wrong input")
+# Initial_Value = 5
+# while Initial_Value >=1:
+#     print(Initial_Value)
+#     Initial_Value = Initial_Value - 1
+# print("Completed")
